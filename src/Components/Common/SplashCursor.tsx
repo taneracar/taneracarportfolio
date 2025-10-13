a3d34facc1f64e1944b656c34f1611625dffa367
taner.acar <taneracar1@icloud.com>
2025-10-13 18:14:12 +0300

    add dummy banner text

--- src/Components/Common/SplashCursor.tsx
+++ src/Components/Common/SplashCursor.tsx
@@ -1512,7 +1512,7 @@ export default function SplashCursor({
   ]);
 
   return (
-    <div className="fixed top-[0] left-0 z-30 pointer-events-none w-full h-full">
+    <div className="fixed top-[0] left-0 z-10 pointer-events-none w-full h-full">
       <canvas
         ref={canvasRef}
         id="fluid"
